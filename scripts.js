3864a0b559962167d8c4dc3782a7d1d1a99095dd
Ashton-Willard <ashtonwillard2021@gmail.com>
2024-11-05 10:44:29 -0500

    This is a message

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// let headers = document.body.getElementsByTagName("h2");
+// for (let header of headers){
+// console.log(header.textContent);
+// }
+
+//To find a specific single node, you can give it an id attribute and use document.getElementById instead//
